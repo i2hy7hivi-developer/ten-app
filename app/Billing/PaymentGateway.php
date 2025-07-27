6f13b6096c00b4978abe80df3386132cdb7512b9
Rhythm Dubey <rhythm@itradicals.com>
2025-07-27 10:22:41 +0530

    Custom dependency injection
    With the currency

--- app/Billing/PaymentGateway.php
+++ app/Billing/PaymentGateway.php
@@ -6,10 +6,15 @@ use Illuminate\Support\Str;
 
 class PaymentGateway
 {
+    public function __construct(
+        private string $currency
+    ){}
+
     public function charge(int $amount)
     {
         return [
             'amount' => $amount,
+            'currency' => $this->currency,
             'transaction_id' => Str::random()
         ];
     }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Billing\PaymentGateway;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +12,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(PaymentGateway::class, function ($app) {
+            return new PaymentGateway('USD');
+        });
     }
 
     /**
